d283b3da1a792ae65109f44e4540627848fccab1
ShubhamFistbumpDigital <shubham.fistbump@gmail.com>
2020-07-23 00:11:53 +0530

    validation inproved

--- js/index.js
+++ js/index.js
@@ -112,5 +112,13 @@ var nameI = $("#name");
                 emailError.html('')
         });
 
+        function restrictAlphabets(e) {
+                var x = e.which || e.keycode;
+                if ((x >= 48 && x <= 57))
+                    return true;
+                else
+                    return false;
+            }
+
 
 onload();
