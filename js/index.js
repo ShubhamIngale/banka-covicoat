4979f558289bfb3731677ee16af8eaf452bec1ca
ShubhamFistbumpDigital <shubham.fistbump@gmail.com>
2020-07-22 23:59:15 +0530

    changes in validation done

--- js/index.js
+++ js/index.js
@@ -19,17 +19,27 @@ var onSubmit = function(token) {
         var nameError = $('.nameError');
         var emailError = $('.emailError');
         var phoneError = $('.phoneError');
+        var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+        const phoneReg = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
 
         if(name === "") {
-                nameError.html('Name required');
+                nameError.html('Please enter your name');
                 grecaptcha.reset();
         }
         else if(phone === '') {
-                phoneError.html('Phone required');
+                phoneError.html('Please enter your phone number');
+                grecaptcha.reset();
+        }
+        else if(phoneReg.test(phone) == false) {
+                phoneError.html('Entered phone number is incorrect');
                 grecaptcha.reset();
         }
         else if(email === '') {
-                emailError.html('Email required');
+                emailError.html('Please enter your email Id');
+                grecaptcha.reset();
+        }
+        else if(reg.test(email) == false) {
+                emailError.html('Entered email Id is incorrect');
                 grecaptcha.reset();
         }
         else {
@@ -101,5 +111,6 @@ var nameI = $("#name");
         emailI.change(function() {
                 emailError.html('')
         });
+
 
 onload();
